4714be1d968fd677a93b22ce0882c9326c5a78fc
zys8119 <770959294@qq.com>
2019-12-15 22:18:53 +0800

    dome

--- application/Dome/controller/IndexController.ts
+++ application/Dome/controller/IndexController.ts
@@ -2,45 +2,45 @@ import applicationController from "../../../UnityFrontUtils/controller/applicati
 import {ServerPublicConfig} from "../../../UnityFrontUtils/config";
 
 export class IndexController extends applicationController {
-        constructor(){
-            super();
-        }
+    constructor(){
+        super();
+    }
 
-        index(){
-            this.$_log("as");
-            console.log("写入成功");
-            this.$_success();
-        }
+    index(){
+        this.$_log("as");
+        console.log("写入成功");
+        this.$_success();
+    }
 
-        axios(){
-            this.$_axios({
-                url:"http://www.baidu.com"
-            }).then(res=>{
-                this.$_success(res.data);
-            });
-        }
+    axios(){
+        this.$_axios({
+            url:"http://www.baidu.com"
+        }).then(res=>{
+            this.$_success(res.data);
+        });
+    }
 
-        dom(){
-            this.$_puppeteer("https://www.baidu.com/s?ie=UTF-8&wd=?",()=>new Promise(resolve=>{
-                let resData = [];
-                resData.push.apply(resData,document.querySelectorAll(".c-container .t a"));
-                let result =  resData.map((el:HTMLAnchorElement)=>({
-                    url:el.href,
-                    value:el.innerText
-                }));
-                resolve(result)
-            })).then(res=>{
-                this.$_success(res)
-            }).catch(err=>{
-                this.$_error(err);
-            });
-        }
+    dom(){
+        this.$_puppeteer("https://www.baidu.com/s?ie=UTF-8&wd=?",()=>new Promise(resolve=>{
+            let resData = [];
+            resData.push.apply(resData,document.querySelectorAll(".c-container .t a"));
+            let result =  resData.map((el:HTMLAnchorElement)=>({
+                url:el.href,
+                value:el.innerText
+            }));
+            resolve(result)
+        })).then(res=>{
+            this.$_success(res)
+        }).catch(err=>{
+            this.$_error(err);
+        });
+    }
 
-        fileStreamDownload(){
-            this.$_fileStreamDownload("http://localhost:8080/public/example.png",false).catch((err)=>{
-                this.$_error(err);
-            });
-        }
+    fileStreamDownload(){
+        this.$_fileStreamDownload("http://localhost:8080/public/example.png",false).catch((err)=>{
+            this.$_error(err);
+        });
+    }
 
     encrypt(){
         this.$_success({
