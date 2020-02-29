5e23e5e282a72273d14442d8d332c963f7c2d6b7
zys8119 <770959294@qq.com>
2020-02-29 15:11:57 +0800

    时间组件

---
+++ Front/src/dashboard/rd/Time/Time.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="Time">{{time}}</div>
+</template>
+
+<script>
+    import { dateFormat } from "vux"
+    export default {
+        name: "Time",
+        title:"时间",
+        data(){
+            return {
+                time:"",
+            }
+        },
+        mounted() {
+            this.time = dateFormat(new Date);
+            setInterval(()=>{
+                this.time = dateFormat(new Date);
+            },1000);
+        }
+    }
+</script>
+
+<style scoped lang="less">
+.Time{
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    color: #ffffff;
+}
+</style>
