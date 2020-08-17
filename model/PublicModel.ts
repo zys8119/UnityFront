b60b566cbb9390882d047c3ff1268a606f9a9370
zys8119 <770959294@qq.com>
2020-08-17 14:06:04 +0800

    mysql 模型

--- model/PublicModel.ts
+++ model/PublicModel.ts
@@ -4,4 +4,8 @@ export default class extends mysql{
     constructor(optionsConfig:object = {}) {
         super(optionsConfig);
     }
+    from(TableName: string, showSqlStr?: boolean): this {
+        console.log(333333)
+        return super.from(TableName, showSqlStr);
+    }
 }
--- model/UserModel.ts
+++ model/UserModel.ts
@@ -1,7 +1,5 @@
 import PublicModel from "./PublicModel";
 
 export default class extends PublicModel{
-    from(TableName: string = "user", showSqlStr?: boolean): this {
-        return super.from(TableName, showSqlStr);
-    }
+
 }
