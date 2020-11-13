993dbfac32c31c1a863aaaa9b65242c0f9a02540
zys8119 <770959294@qq.com>
2020-11-13 17:16:54 +0800

    布局

---
+++ Admin/package/ui/index.js
@@ -0,0 +1,10 @@
+"use strict";
+exports.__esModule = true;
+var vue_1 = require("vue");
+var requireComponent = require.context('.', true, /(\.vue)$/);
+// 注入全局公共组件
+requireComponent.keys().forEach(function (fileName) {
+    var config = requireComponent(fileName);
+    var componentName = fileName.replace(/^\.(.*\/|\/)/, '').replace(/\.\w+$/, '');
+    vue_1["default"].component(componentName, config["default"] || config);
+});
