67ded3a46aa4b10f84537f9e801a81bedc04ff72
zys8119 <770959294@qq.com>
2020-11-19 18:02:49 +0800

    优化

--- Admin/src/store/menus.js
+++ Admin/src/store/menus.js
@@ -1,25 +1,29 @@
-let id = 0;
-const getId = ()=>{
-    id += 1;
-    return id.toString();
-}
-
 let menus = [];
-if(process.env.NODE_ENV !== 'production'){
-    menus = [
-        {
-            title:"系统管理",
-            path:"/system-management/authority-management/menu-management",id:getId(),
-            children:[
-                {title:"权限管理",id:getId(), path:"/system-management/authority-management/menu-management",children:[
-                    {title:"菜单管理",id:getId(), path:"/system-management/authority-management/menu-management"},
-                    {title:"菜单类型管理",id:getId(), path:"/system-management/authority-management/menu-type-management"},
-                    {title:"角色管理",id:getId(), path:"/system-management/authority-management/roles-management"},
-                    {title:"角色分类管理",id:getId(), path:"/system-management/authority-management/roles-type-management"},
-                    {title:"用户管理",id:getId(), path:"/system-management/authority-management/users-management"},
-                ]},
-            ]
-        }
-    ]
-}
+
+/**
+ * 开发环境自定义本地菜单
+ */
+// let id = 0;
+// const getId = ()=>{
+//     id += 1;
+//     return id.toString();
+// }
+//
+// if(process.env.NODE_ENV !== 'production'){
+//     menus = [
+//         {
+//             title:"系统管理",
+//             path:"/system-management/authority-management/menu-management",id:getId(),
+//             children:[
+//                 {title:"权限管理",id:getId(), path:"/system-management/authority-management/menu-management",children:[
+//                     {title:"菜单管理",id:getId(), path:"/system-management/authority-management/menu-management"},
+//                     {title:"菜单类型管理",id:getId(), path:"/system-management/authority-management/menu-type-management"},
+//                     {title:"角色管理",id:getId(), path:"/system-management/authority-management/roles-management"},
+//                     {title:"角色分类管理",id:getId(), path:"/system-management/authority-management/roles-type-management"},
+//                     {title:"用户管理",id:getId(), path:"/system-management/authority-management/users-management"},
+//                 ]},
+//             ]
+//         }
+//     ]
+// }
 export default menus;
