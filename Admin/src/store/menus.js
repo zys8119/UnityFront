28ca96e36f6d09345cba808d3b772f4d93a1fe82
zys8119 <770959294@qq.com>
2020-11-19 15:09:14 +0800

    增加子菜单控制

--- Admin/src/store/menus.js
+++ Admin/src/store/menus.js
@@ -12,12 +12,12 @@ if(process.env.NODE_ENV !== 'production'){
             path:"/system-management/authority-management/menu-management",id:getId(),
             children:[
                 {title:"权限管理",id:getId(), path:"/system-management/authority-management/menu-management",children:[
-                        {title:"菜单管理",id:getId(), path:"/system-management/authority-management/menu-management"},
-                        {title:"菜单类型管理",id:getId(), path:"/system-management/authority-management/menu-type-management"},
-                        {title:"角色管理",id:getId(), path:"/system-management/authority-management/roles-management"},
-                        {title:"角色分类管理",id:getId(), path:"/system-management/authority-management/roles-type-management"},
-                        {title:"用户管理",id:getId(), path:"/system-management/authority-management/users-management"},
-                    ]},
+                    {title:"菜单管理",id:getId(), path:"/system-management/authority-management/menu-management"},
+                    {title:"菜单类型管理",id:getId(), path:"/system-management/authority-management/menu-type-management"},
+                    {title:"角色管理",id:getId(), path:"/system-management/authority-management/roles-management"},
+                    {title:"角色分类管理",id:getId(), path:"/system-management/authority-management/roles-type-management"},
+                    {title:"用户管理",id:getId(), path:"/system-management/authority-management/users-management"},
+                ]},
             ]
         }
     ]
