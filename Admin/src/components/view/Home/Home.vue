0a56fd9a65f81feda22f10992aa90f5b7e5658a6
zys8119 <770959294@qq.com>
2020-11-21 16:53:11 +0800

    优化

--- Admin/src/components/view/Home/Home.vue
+++ Admin/src/components/view/Home/Home.vue
@@ -1,16 +1,36 @@
 <template>
     <div class="Home">
-       <layout-box>
-           <layout-filter-content>
-               首页
-           </layout-filter-content>
-       </layout-box>
+        <div class="row">
+            <layout-box>
+                <layout-filter-content>
+                    <el-form>
+                        <el-form-item label="用户名称：">{{airforce.login.name}}</el-form-item>
+                        <el-form-item label="邮箱：">{{airforce.login.email}}</el-form-item>
+                        <el-form-item label="手机：">{{airforce.login.phone | phone}}</el-form-item>
+                        <el-form-item label="登陆时间：">{{airforce.login.time}}</el-form-item>
+                    </el-form>
+                </layout-filter-content>
+            </layout-box>
+            <layout-box>
+                <layout-filter-content>
+                    <h6>平台日志</h6>
+                </layout-filter-content>
+            </layout-box>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
     name: "Home",
+    filters:{
+        phone(val){
+            if(val){
+                return val.slice(0,3)+"*".repeat(8)
+            }
+            return "暂无";
+        }
+    },
     mounted() {
 
     }
@@ -19,6 +39,11 @@ export default {
 
 <style scoped lang="less">
 .Home{
-
+    .row{
+        display: flex;
+        .LayoutBox{
+            flex: 1;
+        }
+    }
 }
 </style>
