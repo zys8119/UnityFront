9bedfaf711e35915fbc3138ac596bedb732d3c60
zys8119 <770959294@qq.com>
2020-11-24 17:25:28 +0800

    超时时间

--- UnityFrontUtils/config/index.ts
+++ UnityFrontUtils/config/index.ts
@@ -37,7 +37,7 @@ export const ServerConfig =  <ServerOptions>{
     port:81,
     ws_port:82,
     ws_user:{},
-    timeout:8000,
+    timeout:5000,
     debug:true,
     CORS:true,
     Credentials:true,
