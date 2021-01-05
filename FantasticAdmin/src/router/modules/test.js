fdc485893f414253bffd135b97b1a7c151b6b90e
zys8119 <770959294@qq.com>
2021-01-05 14:02:19 +0800

    FantasticAdmin

--- FantasticAdmin/src/router/modules/test.js
+++ FantasticAdmin/src/router/modules/test.js
@@ -1,69 +1,22 @@
 import Layout from '@/layout'
-import EmptyLayout from '@/layout/empty'
 
 export default {
-    path: '/multilevel_menu_example',
+    path: '/test',
     component: Layout,
-    redirect: '/multilevel_menu_example/page',
-    name: 'multilevelMenuExample',
+    redirect: '/test/page',
+    name: 'test',
     meta: {
-        title: '多级导航',
+        title: '测试菜单',
         icon: 'sidebar-menu'
     },
     children: [
         {
             path: 'page',
-            name: 'multilevelMenuExample1',
+            name: 'test_page',
             component: () => import(/* webpackChunkName: 'multilevel_menu_example' */ '@/views/multilevel_menu_example/page'),
             meta: {
-                title: '导航1'
+                title: '测试页面'
             }
-        },
-        {
-            path: 'level2',
-            name: 'multilevelMenuExample2',
-            component: EmptyLayout,
-            redirect: '/multilevel_menu_example/level2/page',
-            meta: {
-                title: '导航2'
-            },
-            children: [
-                {
-                    path: 'page',
-                    name: 'multilevelMenuExample2-1',
-                    component: () => import(/* webpackChunkName: 'multilevel_menu_example' */ '@/views/multilevel_menu_example/level2/page'),
-                    meta: {
-                        title: '导航2-1'
-                    }
-                },
-                {
-                    path: 'level3',
-                    name: 'multilevelMenuExample2-2',
-                    component: EmptyLayout,
-                    redirect: '/multilevel_menu_example/level2/level3/page1',
-                    meta: {
-                        title: '导航2-2'
-                    },
-                    children: [
-                        {
-                            path: 'page1',
-                            name: 'multilevelMenuExample2-2-1',
-                            component: () => import(/* webpackChunkName: 'multilevel_menu_example' */ '@/views/multilevel_menu_example/level2/level3/page1'),
-                            meta: {
-                                title: '导航2-2-1'
-                            }
-                        },
-                        {
-                            path: 'page2',
-                            name: 'multilevelMenuExample2-2-2',
-                            component: () => import(/* webpackChunkName: 'multilevel_menu_example' */ '@/views/multilevel_menu_example/level2/level3/page2'),
-                            meta: {
-                                title: '导航2-2-2'
-                            }
-                        }
-                    ]
-                }
-            ]
         }
     ]
 }
