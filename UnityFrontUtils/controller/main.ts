5ca4408a13aacaef7feba6badb70704d4e837022
zys8119 <770959294@qq.com>
2021-02-18 15:15:13 +0800

    Mock

--- UnityFrontUtils/controller/main.ts
+++ UnityFrontUtils/controller/main.ts
@@ -1,11 +1,31 @@
 import applicationController from "./applicationController"
+import Mock from "./Mock"
 export class main extends applicationController{
     constructor(){
         super();
     }
 
     index(){
-        //控制器url路由解析
-        this.UrlParse();
+        new Promise((resolve,reject) => {
+            resolve();
+        }).then(()=>{
+            //控制器url路由解析
+            this.UrlParse();
+        }).catch(()=>{
+            this.setHeaders({
+                "Access-Control-Allow-Headers":"*",
+            })
+            let next = true;
+            for(let k in Mock){
+                if(k && new RegExp(k).test(this.$_url)){
+                    next = false;
+                    Mock[k].call(this);
+                    break;
+                }
+            }
+            if(next){
+                this.$_success(null,null,0);
+            }
+        })
     }
 }
