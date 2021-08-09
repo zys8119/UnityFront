38dfd15e75ba5a81ca6aaadd591db20b3a5b2b4e
zys8119 <770959294@qq.com>
2021-08-09 12:03:57 +0800

    [add] pdf解析

--- application/Dome/controller/PdfController.ts
+++ application/Dome/controller/PdfController.ts
@@ -3,6 +3,7 @@ import {resolve} from "path"
 import {readFileSync, writeFileSync} from "fs"
 import {inflateSync, deflateSync} from "zlib"
 import {set,merge} from "lodash"
+import {xml2json} from "xml-js"
 export class PdfController extends applicationController {
     filePath = resolve(__dirname,"../../../public/a.pdf");
     fileBuff = readFileSync(this.filePath);
@@ -28,19 +29,33 @@ export class PdfController extends applicationController {
                 keyOld.pop()
             }
         });
+        let markMapKeys = Object.keys(markMap || {});
+        let isXML = markMapKeys.includes("XML");
         let content = (buffStr.split(mark))[1];
         let stream:any = null;
         try{
             stream = this.bufferSplit(buff,"stream")[1]
-            stream = this.bufferSplit(stream,"\r\n")
-            stream = stream.slice(1,stream.length - 1)
-            stream = stream.map((e,k,a)=>{
-                if(k < a.length - 1){
-                    return Buffer.concat([e,Buffer.from("\r\n")])
-                }else{
-                    return e;
-                }
-            });
+            if(isXML){
+                stream = this.bufferSplit(stream,"\n");
+                stream = stream.slice(1,stream.length - 1)
+                stream = stream.map((e,k,a)=>{
+                    if(k < a.length - 1){
+                        return Buffer.concat([e,Buffer.from("\n")])
+                    }else{
+                        return e;
+                    }
+                });
+            }else{
+                stream = this.bufferSplit(stream,"\r\n");
+                stream = stream.slice(1,stream.length - 1)
+                stream = stream.map((e,k,a)=>{
+                    if(k < a.length - 1){
+                        return Buffer.concat([e,Buffer.from("\r\n")])
+                    }else{
+                        return e;
+                    }
+                });
+            }
             stream = Buffer.concat(stream);
         }catch(e){}
         let streamStr = null;
@@ -48,7 +63,7 @@ export class PdfController extends applicationController {
             streamStr = stream.toString()
         }catch(e){}
         let streamDecode = null;
-        let markMapKeys = Object.keys(markMap || {});
+
         try{
             if(markMapKeys.includes("DCTDecode")){
                 streamDecode = deflateSync(stream).toString()
@@ -57,34 +72,108 @@ export class PdfController extends applicationController {
             }
         }catch(e){}
         const result = {
+            // 缓冲器
             buff,
+            // 缓存字符串
             buffStr,
+            // 对象标识符
             key,
+            // 标识符内容
             mark,
+            // 标识map映射
             markMap,
+            // keyName集合
+            markMapKeys,
+            // 内容
             content,
+            // 流
             stream,
+            // 流字符
             streamStr,
+            // 流解码内容
             streamDecode,
         }
         return result
     });
+
     constructor(){
         super()
     }
 
     async index(){
-        this.$_success(await new Promise(resolve1 => {
-            this.fileBuffSplitArray.forEach((obj,k)=>{
-                let stream = obj.stream;
-                if(stream){
-                    writeFileSync(resolve(__dirname,"a_"+k+".jpg"),stream)
-                }
-            });
-            resolve1()
-        }));
+        this.$_success(await this.parse());
     }
 
+    /**
+     * 解析pdf
+     */
+    async parse(){
+        return {
+            info:await this.getInfo()
+        }
+    }
+
+    /**
+     * 获取信息
+     */
+    async getInfo(){
+        let info:any = {};
+        try{
+            const Root:any = this.fileBuffSplitArray.find(e=>e.markMap && e.markMap.Root) || {};
+            const RootObj:any = this.fileBuffSplitArray.find(e=>e.key === this.getObjName(Root.markMap.Root)) || {};
+            info.Root = RootObj.markMap;
+            try{
+                const Metadata:any = this.fileBuffSplitArray.find(e=>e.key === this.getObjName(RootObj.markMap.Metadata)) || {};
+                if(Metadata.markMapKeys.includes("XML")){
+                    info.Root.Metadata = JSON.parse(xml2json(Metadata.streamStr,{
+                        compact:true,
+                        trim:true,
+                    }))
+                }
+            }catch(e){}
+            try{
+                const Pages:any = this.fileBuffSplitArray.find(e=>e.key === this.getObjName(RootObj.markMap.Pages)) || {};
+                info.Pages = Pages.markMap;
+            }catch(e){}
+        }catch(e){}
+
+        return info;
+    }
+
+    /**
+     * 获取对象名称
+     * @param name
+     */
+    getObjName(name){
+        return name.replace(/R/img,"obj");
+    }
+    /**
+     * 获取时间
+     * @param str
+     */
+    getDate(str){
+        return /(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})(\d{2})/.exec(str).slice(1).map((e,k)=>{
+            switch(k){
+                case 0:
+                    return `${e}年` ;
+                case 1:
+                    return `${e}月` ;
+                case 2:
+                    return `${e}日` ;
+                case 4:
+                    return `${e}时` ;
+                case 5:
+                    return `${e}分` ;
+                case 6:
+                    return `${e}秒` ;
+            }
+        }).join("")
+    }
+
+    /**
+     * 获取key
+     * @param key
+     */
     getKey(key){
         try{
             const arr = key.replace(/\n|>>|<</img,"").split(" ")
