2e667905d33d8c57f33fb822e4a02a65a6ff3047
zys8119 <770959294@qq.com>
2021-12-03 10:59:54 +0800

    [add] node ts

--- UnityFrontUtils/typeStript/declares.ts
+++ UnityFrontUtils/typeStript/declares.ts
@@ -3,7 +3,3 @@ import {PathLike} from "fs";
 declare module "fs" {
     export function readFileSync<T = string>(path: PathLike | number, options?: { encoding?: null; flag?: string; } | null | T): Buffer & T;
 }
-
-declare interface PromiseConstructor {
-    new <T>(executor: (resolve: (value?: T | PromiseLike<T>) => void, reject: (reason?: any) => void) => void): Promise<T>;
-}
