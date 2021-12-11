962d13a056feacf1bb8de6644cf5b7b9e9e9c62b
zys8119 <zys112425>
2021-12-11 14:11:20 +0800

    [add] 北外网

--- application/Dome/controller/IconfontController.ts
+++ application/Dome/controller/IconfontController.ts
@@ -61,22 +61,23 @@ export class IconfontController extends applicationController{
     async beiwai(index = 0){
         console.log("=============================================")
         const item = {
-            "outlineCode": "1627536666051",
+            "outlineCode": "1627536666049",
             "curriculumCode": "ZK_BWME3020_20210730203203938",
             "parentCode": "1594610914500628978",
-            "name": "第2节 网站界面设计框架与流程",
-            "outlineOrder": 605,
+            "name": "第4节 网站目标与客户体验设计",
+            "outlineOrder": 609,
             "nodeType": "resource",
             "type": 400004,
-            "src": "proxy/resource/590540681223606272",
-            "times": 5.32,
-            "id": "167018",
-            "tid": "1685013",
+            "src": "proxy/resource/591561725505966080",
+            "times": 16.53,
+            "id": "167037",
+            "tid": "1685016",
             "suffix": "_360p.m3u8",
-            "courseElementId": 1685013,
+            "courseElementId": 1685016,
             "courseElementState": "0",
             "learnerCourseId": 4812739
         }
+        const cookie =  "INGRESSCOOKIE=8068b2df631e8d2d268530503cbde8f1; JSESSIONID=C7B48DF109DA7730B531A9CA5DEDA007"
         const config = {
             userId: 629700,
             learnCourseId: item.learnerCourseId,
@@ -87,9 +88,10 @@ export class IconfontController extends applicationController{
             clientSource: 190010110,
             stayTime:60,
         }
-        const learnDetails = new Array(config.stayTime).fill(0).map((e,k)=>k+index);
+        const currIndex = index + (index === 0 ? 0 :1);
+        const learnDetails = new Array(config.stayTime).fill(0).map((e,k)=>k+currIndex);
         const times = item.times*60;
-        const lastLearnTime = index + config.stayTime;
+        const lastLearnTime = currIndex + config.stayTime;
         const params = {
             userId: config.userId,
             loginName:config.loginName,
@@ -103,7 +105,11 @@ export class IconfontController extends applicationController{
         const res = (await this.$_axios({
             method:"post",
             url:"https://study.ebeiwai.com/ws/studyservice/sendLearnDetailToMassdataTmp",
-            params
+            params,
+            // withCredentials:true,
+            // headers:{
+            //     cookie
+            // }
         })).data
         const params2 = {
             userId: config.userId,
@@ -120,6 +126,10 @@ export class IconfontController extends applicationController{
             method:"post",
             url:"https://study.ebeiwai.com/ws/studyservice/sendLearnDetailToMassdataTmp",
             params:params2,
+            // withCredentials:true,
+            // headers:{
+            //     cookie,
+            // },
             paramsSerializer(params){
                 const result = Object.entries(params).map(([k,v]:[string,Array<number> & string])=>{
                     if(Object.prototype.toString.call(v) === "[object Array]"){
@@ -133,27 +143,30 @@ export class IconfontController extends applicationController{
         console.log(index, times, index < times,learnDetails)
         console.log(res, res2)
         if(lastLearnTime < times){
-            await this.beiwai(lastLearnTime)
+            return await this.beiwai(lastLearnTime)
         }else {
             this.$_success("学习完成");
         }
+    }
 
-
-        // const res3 = (await this.$_axios({
-        //     method:"post",
-        //     url:"https://study.ebeiwai.com/ws/studyservice/getStandedCourseOutline",
-        //     params:{
-        //         code: "ZK_BWME3020_20210730203203938",
-        //         type: 1,
-        //         courseId: 4204,
-        //         learnCourseId: 4812739,
-        //     }
-        // })).data
-        // console.log(this.toTree(res3.outlines,{
-        //     id:"outlineCode",
-        //     parent:"parentCode",
-        // }));
-        // console.log(JSON.stringify(res3.outlines.find(e=>e.outlineCode === "1627536666051"),null, 4))
-
+    async beiwaicourse(){
+        const res3 = (await this.$_axios({
+            method:"post",
+            url:"https://study.ebeiwai.com/ws/studyservice/getStandedCourseOutline",
+            params:{
+                code: "ZK_BWME3020_20210730203203938",
+                type: 1,
+                courseId: 4204,
+                learnCourseId: 4812739,
+            }
+        })).data
+        console.log(JSON.stringify(res3.outlines
+            .find(e=>e.outlineCode === "1627536111167"), null, 4));
+        const tree = this.toTree(res3.outlines,{
+            id:"outlineCode",
+            parent:"parentCode",
+        })
+        console.log(tree)
+        this.$_success();
     }
 }
