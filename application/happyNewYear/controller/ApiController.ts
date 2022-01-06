f0d0e52503fea5b10d826fbfde50866a67c21e27
zys8119 <770959294@qq.com>
2022-01-06 11:36:00 +0800

    [add] happyNewYear

--- application/happyNewYear/controller/ApiController.ts
+++ application/happyNewYear/controller/ApiController.ts
@@ -5,21 +5,30 @@ export class ApiController extends applicationController{
         super();
     }
 
+    /**
+     * 创建数据
+     */
     async index(){
         await this.DB().insert("happyNewYear",[
             {
                 nickname:"测试用户"+Date.now(),
                 openid:Date.now(),
-                headimgurl:null,
+                headimgurl:"https://t7.baidu.com/it/u=1819248061,230866778&fm=193&f=GIF",
             }
         ]).query()
         this.$_success()
     }
 
+    /**
+     * 获取签到人员列表
+     */
     async getSignInList(){
         this.$_success(await new this.$sqlModel.happyNewYear().select().from().query())
     }
 
+    /**
+     * 更新墙展示
+     */
     async wallChange(){
         await new this.$sqlModel.happyNewYear().update({
             wall:0,
@@ -28,5 +37,19 @@ export class ApiController extends applicationController{
         }).query()
         console.log(this.$_body)
         this.$_success()
+    }
+
+    /**
+     * 抽奖结束
+     */
+    async luckStop(){
+        this.$_success();
+    }
+
+    /**
+     * 抽奖开始
+     */
+    async luckStart(){
+        this.$_success();
     }
 }
