7a5d5def0ea2f55cfcbbed0e1a6495e1f3c0c5cb
zys8119 <770959294@qq.com>
2022-03-28 17:12:00 +0800

    [add] 阿里图标自动化

--- application/Dome/controller/IconfontController.ts
+++ application/Dome/controller/IconfontController.ts
@@ -1,61 +1,115 @@
 import {applicationController} from "../../../UnityFrontUtils/controller/applicationController";
-import {LaunchOptions, BrowserLaunchArgumentOptions, BrowserConnectOptions, Page, Browser} from "puppeteer"
-import {parse} from "url"
+import puppeteer, {
+    Page,
+} from "puppeteer"
 export class IconfontController extends applicationController{
     constructor() {
         super();
     }
 
+    async pageSuccess(){
+        this.$_success()
+    }
+
     async index(){
-        const res = await this.$_puppeteer("https://www.iconfont.cn/login",<LaunchOptions & BrowserLaunchArgumentOptions & BrowserConnectOptions & {
-            jsContentFn:any
-            jsContentBeforeFn:any
-        }>{
-            slowMo:10,
-            jsContentBeforeFn:async (page:Page)=>{
-                let bool = false
-                page.on("response", async e=>{
-                    if(/detail\.json/.test(e.url()) && bool){
-                        // 返回项目数据
-                        try {
-                            const icons = (await e.json()).data.icons
-                            await page.evaluateHandle(function (icons){
-                                (<any>window).$_puppeteer = icons
-                            }, icons)
-                            console.log("返回项目数据")
-                        }catch (e){
-                            console.error(e.message)
-                        }
-                    }
-                })
-                // 输入密码
-                await new Promise(r=>setTimeout(r,500))
-                await page.type("#userid",this.$_query.userid,{delay:0});
-                await page.type("#password",this.$_query.password, {delay:0});
-                await page.tap("#login-form > div:nth-child(4) > button");
-                console.log("登陆")
-                // 登陆成功跳转
-                await new Promise(r=>setTimeout(r,500))
-                // 进入项目管理
-                await page.hover("#magix_vf_header > header > div > nav > ul > li:nth-child(4)")
-                await page.tap("#magix_vf_header > header > div > nav > ul > li:nth-child(4) > ul > li:nth-child(4) > a")
-                await new Promise(r=>setTimeout(r,6000))
-                console.log("进入项目管理")
-                // 选中指定项目
-                await page.tap("#mx_16 > div.page-manage-left > div > div:nth-child(2) > div.nav-lists.J_scorll_project_corp > div:nth-child(3)")
-                console.log("选中指定项目")
-                bool = true;
-                await new Promise(r=>setTimeout(r,500))
-                console.log(5)
-            },
-            jsContentFn:()=>{
-                return Promise.resolve((<any>window).$_puppeteer)
+        const {username, password, projectName} = this.$_query;
+        if(!username || !password || ! projectName){
+            return this.$_error("参数有误")
+        }
+        let projectId = null;
+        let ownProjectsObj = null;
+        let detailJson = null;
+        let manageUrl = null;
+        let projectUrl = null;
+        try {
+            const puppeteer = require('puppeteer');
+            const load = async (page, selectors)=> {
+                return await page.evaluateHandle((selectors)=>{
+                    return new Promise<void>(resolve=>{
+                        const timeout = ()=> setTimeout(()=>{
+                            if(document.querySelector(selectors)){
+                                setTimeout(()=>{
+                                    resolve()
+                                })
+                            }else {
+                                timeout()
+                            }
+                        },200)
+                        timeout()
+                    })
+                }, selectors)
             }
-        }).catch(err=>{
-            this.$_error(err)
-        })
-        console.log(res,666)
-        this.$_success(res)
+            const pageRequest = async (page:Page, browser, resolve)=>{
+                try {
+                    page.on("response", async (e)=>{
+                        const url = e.url();
+                        switch (true) {
+                            case /www\.iconfont\.cn\/$/.test(url):
+                                console.log("请稍等")
+                                const page2 = await browser.newPage();
+                                await page2.goto(e.url());
+                                await pageRequest(page2, browser, resolve);
+                                await load(page2,"#magix_vf_header > header > div > nav > ul > li:nth-child(3)");
+                                await page2.hover("#magix_vf_header > header > div > nav > ul > li:nth-child(3)")
+                                await page2.tap("#magix_vf_header > header > div > nav > ul > li:nth-child(3) > ul > li:nth-child(6)")
+                                console.log("进入项目")
+                                break;
+                            case /manage\/index.*manage_type/.test(url):
+                                manageUrl = url
+                                break;
+                            case /myprojects\.json.*isown_create=1/.test(url):
+                                // 获取我的项目信息
+                                if(!ownProjectsObj){
+                                    console.log("请稍等")
+                                    ownProjectsObj = (await e.json()).data.ownProjects.find(e=>e.name === projectName) || {}
+                                    projectId = ownProjectsObj.id;
+                                    const page3 = await browser.newPage();
+                                    projectUrl = `${manageUrl}&projectId=${projectId}&__${encodeURIComponent(projectName)}__=true`;
+                                    await pageRequest(page3, browser, resolve);
+                                    page3.goto(projectUrl);
+                                    console.log("进入项目：", projectName)
+                                }
+                                break;
+                            case /detail.json/.test(url):
+                                if(e.frame().url() === projectUrl){
+                                    console.log("图标获取成功")
+                                    const res = (await e.json()).data
+                                    if(!detailJson && res.project.id === projectId){
+                                        detailJson = res;
+                                        browser.close()
+                                        resolve(res);
+                                    }
+                                }
+                                break;
+                        }
+                    });
+                }catch (e){}
+            }
+            const res = await new Promise<any>(resolve => {
+                puppeteer.launch({
+                    // headless:false,
+                    // devtools:true,
+                }).then(async browser=>{
+                    let page = await browser.newPage();
+                    await pageRequest(page, browser, resolve);
+                    await page.goto("https://www.iconfont.cn/login");
+                    console.log("请稍等")
+                    await load(page,"#userid");
+                    await page.type("#userid",this.$_query.username,{delay:0});
+                    await page.type("#password",this.$_query.password, {delay:0});
+                    await page.tap("#login-form > div:nth-child(4) > button");
+                    console.log("登陆成功")
+                }).catch(err=>{
+                    console.log(err.message, 33333)
+                })
+            }).catch(err=>{
+                console.log(err,66666)
+            })
+            this.$_success(res)
+        }catch (e){
+            // this.$_error(e.message)
+        }
+
     }
 
     async beiwai(index = 0){
