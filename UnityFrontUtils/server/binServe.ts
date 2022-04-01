25d0c2dfb8034ee6d07c90ec58b08673e4ea9ece
zys8119 <770959294@qq.com>
2022-04-01 22:02:51 +0800

    [add] 新增脚手架命令

--- UnityFrontUtils/server/binServe.ts
+++ UnityFrontUtils/server/binServe.ts
@@ -2,30 +2,49 @@ import {version} from "../../package.json"
 const command = require("ncommand")
 export default ()=>{
     if(process.argv[2]){
-        new command()
-            .Commands({
-                log:["-v","查看版本"],
-                callback() {
-                    this.console
-                        .color(function (){
-                            this.log("当前版本号：")
-                                .info(`v ${version}`)
-                        })
-                }
-            })
-            .Commands({
-                log:["-config","配置文件"],
-                callback(e, a) {
-                    process.env.ufConfigPath = a[0] || "";
-                    return true
-                }
-            })
-            .Commands({
-                log:["-h","查看帮助"],
-                callback(e, a) {
+        let isHelp = false;
+        const help =()=>{
+            isHelp = true;
+            initCommand()
+        }
+        const initCommand = ()=> {
+            const c = new command()
+                .Commands({
+                    log:["-v","查看版本"],
+                    callback() {
+                        this.console
+                            .color(function (){
+                                this.log("当前版本号：")
+                                    .success(`v ${version}`)
+                            })
+                    }
+                })
+                .Commands({
+                    log:["-config","...info('<configPath>')","配置文件"],
+                    callback(e, a) {
+                        process.env.ufConfigPath = a[0] || "";
+                        return true
+                    }
+                })
 
-                }
-            })
-            .init()
+
+
+            if(!isHelp){
+                c.Commands({
+                    log:["-help","查看帮助"],
+                    callback() {
+                        help()
+                    }
+                })
+                .Commands({
+                    log:["-h","查看帮助"],
+                    callback() {
+                        help()
+                    }
+                })
+            }
+            c.init()
+        }
+        initCommand();
     }
 }
