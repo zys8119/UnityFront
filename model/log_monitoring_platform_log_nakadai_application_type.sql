37d8220f58cf6b6828504b21e4df6bff84dd1482
zys8119 <770959294@qq.com>
2022-04-28 11:32:59 +0800

    [fix] 日志监控

---
+++ model/log_monitoring_platform_log_nakadai_application_type.sql
@@ -0,0 +1,4 @@
+insert into log-monitoring-platform.log_nakadai_application_type (id, name, is_del)
+values  ('1651116221920124', 'h5', 1),
+        ('1651116226050664', '微信小程序', 1),
+        ('1651116245420498', 'un-app', 1);
