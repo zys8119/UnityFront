381fad1256ff066d5426b7ad792dabf424c1bd7c
zys8119 <770959294@qq.com>
2022-04-28 11:33:29 +0800

    [fix] 日志监控

---
+++ model/log_monitoring_platform_menu.sql
@@ -0,0 +1,16 @@
+insert into log-monitoring-platform.menu (id, name, url, parent, type, is_child_page, is_del)
+values  ('160576915126284', '系统管理', '/system-management/authority-management/menu-management', null, '1605505190793', 1, 1),
+        ('1605769190027186', '权限管理', '/system-management/authority-management/menu-management', '160576915126284', '1605505190793', 1, 1),
+        ('160576927459951', '菜单管理', '/system-management/authority-management/menu-management', '1605769190027186', '1605505190793', 1, 1),
+        ('1605769287136368', '菜单类型管理', '/system-management/authority-management/menu-type-management', '1605769190027186', '1605505190793', 1, 1),
+        ('1605769296852808', '角色管理', '/system-management/authority-management/roles-management', '1605769190027186', '1605505190793', 1, 1),
+        ('1605769306949211', '角色分类管理', '/system-management/authority-management/roles-type-management', '1605769190027186', '1605505190793', 1, 1),
+        ('1605772354630210', '角色权限设置', '/system-management/authority-management/roles-permission-settings', '1605769190027186', '1605505190793', 1, 2),
+        ('1605769095259843', '首页', '/home', null, '1605505190793', 1, 1),
+        ('1605769320496678', '用户管理', '/system-management/authority-management/users-management', '1605769190027186', '1605505190793', 1, 1),
+        ('1605770967404849', '用户中心', '/userInfo', null, '1605505190793', 1, 2),
+        ('1605778715087343', '用户注册页面', '/User/Auth/register', null, '1605505190793', 2, 1),
+        ('160577893512851', '角色权限设置', '/system-management/authority-management/roles-permission-settings', '1605769190027186', '1605505190793', 2, 1),
+        ('1605779531522659', '用户中心', '/userInfo', null, '1605505190793', 2, 1),
+        ('1651114018272393', '应用管理', '/ApplicationManagement/applicationType', null, '1605505190793', 1, 1),
+        ('1651114074973726', '应用类型', '/ApplicationManagement/applicationType', '1651114018272393', '1605505190793', 1, 1);
---
+++ model/log_monitoring_platform_menu_type.sql
@@ -0,0 +1,2 @@
+insert into log-monitoring-platform.menu_type (id, name, type, is_del)
+values  ('1605505190793', '后台菜单', 1, 1);
