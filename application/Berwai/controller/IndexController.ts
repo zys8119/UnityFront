6aeeda7bbfd096a3fe5b1fdeeaf14ab39ab96347
zys8119 <770959294@qq.com>
2022-12-07 10:08:02 +0800

    [add] 北外网测试题目答案自动获取

--- application/Berwai/controller/IndexController.ts
+++ application/Berwai/controller/IndexController.ts
@@ -56,74 +56,82 @@ export class IndexController extends applicationController{
     }
 
     async getCourseAnswer (courseInof, page, reg_name){
-        let res:any = courseInof
-        console.log("课程", courseInof)
-        await page.goto(`https://appd10.beiwaionline.com/learnerfore/learnerfore/gotoStudyCenter/${res[0].code}`)
-        await page.tap("body > div:nth-child(1) > div.href-btn > button")
-        await page.waitForSelector("#contentFclearId > aside > hgroup.course_details > title")
-        res = await (await page.evaluateHandle((reg)=>{
-            return Array.apply(null,document.querySelectorAll(".menu a .child-text")).map(e=> {
-                return {
-                    url:e.parentNode.href,
-                    name:e.innerText
-                }
-            }).filter(e=>new RegExp(reg, 'img').test(e.name))
-        },reg_name)).jsonValue()
-        console.log("练习栏目", res)
-        await page.goto(res[0].url)
-        await page.waitForSelector("#app > div > div.content > div.content-part > div > iframe")
-        res = await (await page.evaluateHandle(()=>{
-            const a:HTMLIFrameElement = document.querySelector("#app > div > div.content > div.content-part > div > iframe")
-            return a.src
-        })).jsonValue()
-        const pageUrl = res
-        console.log("练习页面地址", res)
-        await page.goto(res)
-        res = await (await page.evaluateHandle((reg)=>{
-            return {
-                url:location.href,
-                code:location.pathname.replace(/^.+\//,'')
+        try {
+            let res:any = courseInof
+            console.log("课程", courseInof)
+            await page.goto(`https://appd10.beiwaionline.com/learnerfore/learnerfore/gotoStudyCenter/${res[0].code}`)
+            await page.tap("body > div:nth-child(1) > div.href-btn > button")
+            await page.waitForSelector("#contentFclearId > aside > hgroup.course_details > title")
+            res = await (await page.evaluateHandle((reg)=>{
+                return Array.apply(null,document.querySelectorAll(".menu a .child-text")).map(e=> {
+                    return {
+                        url:e.parentNode.href,
+                        name:e.innerText
+                    }
+                }).filter(e=>new RegExp(reg, 'img').test(e.name))
+            },reg_name)).jsonValue()
+            console.log("练习栏目", res)
+            if(!res[0]){
+                return []
             }
-        })).jsonValue()
-        console.log("重定向后的练习页面地址", res)
-        console.log("练习页面id标识", res.code)
-        res = await (await page.evaluateHandle((code)=>{
-            return fetch(`https://quiz.ebeiwai.com/icourse/fore/myselftest/newView/${code}`).then(res=>{
-                return res.text()
-            }).then(text=>{
-                return /您访问的页面出错了/.test(text)
-            })
-        }, res.code)).jsonValue()
-        console.log("判断是否存在答案", res)
-        if(res){
-            // 答案不存在
-            await page.waitForSelector("#submitRecord")
+            await page.goto(res[0].url)
+            await page.waitForSelector("#app > div > div.content > div.content-part > div > iframe")
             res = await (await page.evaluateHandle(()=>{
-                // @ts-ignore
-                return Object.fromEntries($("form").serialize().split('&').map(e=>e.split('=')))
+                const a:HTMLIFrameElement = document.querySelector("#app > div > div.content > div.content-part > div > iframe")
+                return a.src
             })).jsonValue()
-            console.log("提交的练习数据", res)
-            res = await (await page.evaluateHandle((data)=>{
-                const formData = new FormData()
-                Object.entries(data).forEach(ee=>{
+            const pageUrl = res
+            console.log("练习页面地址", res)
+            await page.goto(res)
+            res = await (await page.evaluateHandle((reg)=>{
+                return {
+                    url:location.href,
+                    code:location.pathname.replace(/^.+\//,'')
+                }
+            })).jsonValue()
+            console.log("重定向后的练习页面地址", res)
+            console.log("练习页面id标识", res.code)
+            res = await (await page.evaluateHandle((code)=>{
+                return fetch(`https://quiz.ebeiwai.com/icourse/fore/myselftest/newView/${code}`).then(res=>{
+                    return res.text()
+                }).then(text=>{
+                    return /您访问的页面出错了/.test(text)
+                })
+            }, res.code)).jsonValue()
+            console.log("判断是否存在答案", res)
+            if(res){
+                // 答案不存在
+                await page.waitForSelector("#submitRecord")
+                res = await (await page.evaluateHandle(()=>{
                     // @ts-ignore
-                    formData.append(ee[0], ee[1])
-                })
-                return fetch("https://quiz.ebeiwai.com/fore/orgquiz/saveLearnerAnswer", {
-                    "body":formData ,
-                    "method": "POST",
-                }).then(res=>{
-                    return res.json()
-                })
-            }, res)).jsonValue()
-            console.log("提交练习的接口响应", res)
-            await page.goto(pageUrl)
+                    return Object.fromEntries($("form").serialize().split('&').map(e=>e.split('=')))
+                })).jsonValue()
+                console.log("提交的练习数据", res)
+                res = await (await page.evaluateHandle((data)=>{
+                    const formData = new FormData()
+                    Object.entries(data).forEach(ee=>{
+                        // @ts-ignore
+                        formData.append(ee[0], ee[1])
+                    })
+                    return fetch("https://quiz.ebeiwai.com/fore/orgquiz/saveLearnerAnswer", {
+                        "body":formData ,
+                        "method": "POST",
+                    }).then(res=>{
+                        return res.json()
+                    })
+                }, res)).jsonValue()
+                console.log("提交练习的接口响应", res)
+                await page.goto(pageUrl)
+            }
+            await page.waitForSelector(".header")
+            res = await (await page.evaluateHandle(()=>{
+                return Array.apply(Array, document.querySelectorAll(".analysis-true")).map(e=>({value:e.innerText, name:e.parentNode.parentNode.querySelector('.question .question').innerText}))
+            })).jsonValue()
+            console.log("最终答案", res)
+            return res
+        }catch (e) {
+            return []
         }
-        await page.waitForSelector(".header")
-        res = await (await page.evaluateHandle(()=>{
-            return Array.apply(Array, document.querySelectorAll(".analysis-true")).map(e=>({value:e.innerText, name:e.parentNode.parentNode.querySelector('.question .question').innerText}))
-        })).jsonValue()
-        console.log("最终答案", res)
-        return res
+
     }
 }
